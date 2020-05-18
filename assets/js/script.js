2a44b8a093dcaaf080c6f694a030bfc98da4a6d2
Emil Ekström <ekstromemil@gmail.com>
2020-05-19 01:56:47 +0300

    bounce animation & responsive navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navbar-logo-img").style.height = "55px";
-    //document.getElementById("navbar-links-ul").style.fontSize = "20px";
-  } else {
-    document.getElementById("navbar-logo-img").style.height = "120px";
-    //document.getElementById("navbar-links-ul").style.fontSize = "35px";
-  }
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar-logo-img").style.height = "55px";
+        //document.getElementById("navbar-links-ul").style.fontSize = "20px";
+    } else {
+        document.getElementById("navbar-logo-img").style.height = "120px";
+        //document.getElementById("navbar-links-ul").style.fontSize = "35px";
+    }
+}
+
+function changeMenu(x) {
+    x.classList.toggle("change"); //Change the menu to a cross, and vice versa
+
+    if (document.getElementById("navbar-links").style.visibility == "visible") {
+
+        document.getElementById("navbar-links").style.opacity = "0";
+        document.getElementById("navbar-links").style.visibility = "hidden";
+        document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, 0.00)";
+
+    } else {
+
+        document.getElementById("navbar-links").style.opacity = "1";
+        document.getElementById("navbar-links").style.visibility = "visible";
+        document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, 0.61)";
+
+    }
+
 }
