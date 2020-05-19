486dfba4ec9af86825d40cc5f7489237bedb6d46
Emil Ekström <ekstromemil@gmail.com>
2020-05-19 06:05:39 +0300

    Time for 0.1.0 BETA

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,11 @@ function changeMenu(x) {
         document.getElementById("navbar-links").style.visibility = "hidden";
         document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, 0.00)";
 
+        setTimeout(function(){ document.getElementById("navbar-links").style.position = "absolute"; }, 0600);
+
     } else {
 
+        document.getElementById("navbar-links").style.position = "static";
         document.getElementById("navbar-links").style.opacity = "1";
         document.getElementById("navbar-links").style.visibility = "visible";
         document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, 0.61)";
